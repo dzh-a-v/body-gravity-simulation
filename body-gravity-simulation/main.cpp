ad6c44a67c028453793704880e9a39ae835687da
Lurtced <Lurtced@yandex.ru>
2025-12-04 17:26:28 +0300

    changed dt to 0.001 s

--- body-gravity-simulation/main.cpp
+++ body-gravity-simulation/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     Simulation sim;
-    sim.dt = 0.01; // 1 секунда на шаг
+    sim.dt = 0.001; // 0.001 секунда на шаг дифференциирования
 
     // Земля (массивная, в центре)
     sim.addBody(Body(5.97e24, 6.37e6, { 0, 0 }, { 0, 0 }));
@@ -13,7 +13,7 @@ int main() {
 
     ConsoleOutput out;
 
-    for (int step = 0; step < 1000000; ++step) {
+    for (int step = 0; step < 10000000; ++step) {
         if (sim.hasCollision()) {
             std::cout << "Collision happend at t = " << sim.time << " s!\n";
             sim.isFinished = true;
@@ -22,7 +22,7 @@ int main() {
 
         sim.step();
 
-        if (step % 50 == 0) {
+        if (step % 100000 == 0) {
             out.render(sim);
         }
     }
