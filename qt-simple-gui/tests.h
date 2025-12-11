fbcf380fbabf1dc08c20ba59e0dd879c87a682fa
dzh-a-v <metroexoduslover@gmail.com>
2025-12-12 01:14:49 +0300

    removed unsued file `tests.h`

--- qt-simple-gui/tests.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct Tests {
-    static void test_equilateral_triangle();
-    static void test_earth_satellite();
-    static void test_earth_satellite_testParticle();
-    static void test_perturbation_small_mass();
-};
